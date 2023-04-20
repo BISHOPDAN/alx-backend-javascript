787ce9aa0aac4df58db85ee468e40759050f21e5
Your Name <you@example.com>
2023-04-20 14:03:03 +0100

    project in ES6 classes

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,7 @@
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const sizes = [19, 20, 34];
+  const rooms = sizes.map(size => new ClassRoom(size));
+  return rooms;
+}
